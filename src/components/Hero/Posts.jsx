c25750459d17a1094b5913cf284bc50fc994c86b
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-03-15 06:00:33 +0500

    first commit

---
+++ src/components/Hero/Posts.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("data/data.json")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="posts">
+        <div className="title">
+          <h1>Our Latest Posts</h1>
+          <form>
+            <input required type="search" placeholder="Search" id="search" />
+            <button htmlFor="search">🔍</button>
+          </form>
+        </div>
+        <div className="cards">
+          {posts.map((post) => (
+            <div className="card">
+              <div className="card-img"></div>
+              <p className="id">ID: {post.id}</p>
+              <h5>{post.body}</h5>
+              <p className="data">Floyd Miles 3 Days Ago</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
